73f1ce28e368c33f3bcca7a152db042174a18e40
anzani-hun <jerikostreet@gmail.com>
2024-04-17 12:28:38 +0200

    Add project files.

---
+++ Csharp-Levesek-NemethZ/Adatbazis.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Threading.Tasks;
+
+using MySqlConnector;       //mysqldata nuget
+
+namespace Csharp_Levesek_NemethZ
+{
+    class Adatbazis
+    {
+        MySqlConnection connection = null;
+        MySqlCommand sql = null;
+
+        public Adatbazis()
+        {
+            string connectionString = "server=localhost;userid=root;password=;database=etelek";
+
+            connection = new MySqlConnection(connectionString);
+            connection.Open();
+
+            sql = connection.CreateCommand();
+            sql.CommandText = "SELECT * FROM levesek;";
+
+
+        }
+
+
+        public List<Levesek> LevesekBetoltese()       //lista a levesek betöltésésre
+        {
+            List<Levesek> levesek = new List<Levesek>();
+
+            MySqlDataReader reader = sql.ExecuteReader();
+
+            while (reader.Read())
+            {
+                Levesek leves = new Levesek(reader.GetString("megnevezes"), reader.GetInt32("kaloria"), reader.GetDecimal("feherje"), reader.GetDecimal("zsir"), reader.GetDecimal("szenhidrat"), reader.GetDecimal("hamu"), reader.GetDecimal("rost"));
+                levesek.Add(leves);
+            }
+            return levesek;
+        }
+
+
+
+        public class Item
+        {
+            public int Id { get; set; }
+            public string Megnevezes { get; set; }
+        }
+
+
+    }
+}
---
+++ Csharp-Levesek-NemethZ/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using MySqlConnector;       //mysqldata nuget
+
+
+
+
+namespace Csharp_Levesek_NemethZ
+{
+    internal class Program
+    {
+        //kívűl kell meghívni
+        static Adatbazis db = new Adatbazis();
+        static List<Levesek> levesek = db.LevesekBetoltese();
+
+
+        static void Main(string[] args)
+        {
+
+            //1. feladat:
+            Console.WriteLine("001. kérdés: Az adattáblában hány leves szerepel? ");
+            Console.WriteLine($"Az adatbázisban [{levesek.Count} db] leves szerepel.\n\n");
+
+          
+
+            //2. feladat:
+            Console.WriteLine("002. kérdés: Melyik levesben van a legtöbb kalória? ");
+
+            int legtobbKaloria = 0;
+            foreach (Levesek item in levesek)
+            {
+                if (item.Kaloria > legtobbKaloria)
+                {
+                    legtobbKaloria = item.Kaloria;
+                }
+            }
+            Console.WriteLine($"A legtöbb kalória {legtobbKaloria}.\n\n");
+
+
+            
+            //3. feladat:
+            Console.WriteLine("003. kérdés: Írasd ki azokat a leveseket, amelyek nevében nem szerepel a leves szó! ");
+            foreach (Levesek item in levesek)
+            {
+                if (!item.Megnevezes.Contains("leves"))
+                {
+                    Console.WriteLine($"Aminek a nevében nem szerepel a leves szó a(z): {item.Megnevezes}");
+                }
+            }
+
+
+
+
+            Console.ReadKey();
+        }
+
+}
+}
